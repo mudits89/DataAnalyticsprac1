72e8ea269211fe606e8db160020e551e161923aa
mudits89 <mudits_89@hotmail.com>
2018-08-14 10:01:55 +0800

    Add files via upload

---
+++ dataprocessingwhitewine.py
@@ -0,0 +1,50 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('winequality-white.csv')
+dataframe = pd.read_csv("C:/Users/mudit/Desktop/Data Analytics/Wine details/winequality-white.csv",delimiter =";")
+print(dataframe.head())
+dataframe.to_csv('winequality-white1.csv',index = False)
+dataset1 = pd.read_csv('winequality-white1.csv')
+X = dataset1.iloc[:, :-1].values
+y = dataset1.iloc[:, 11].values.astype(np.float64)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+
+#Building the optimal model using the backward elimination process
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((4898,1)).astype(float), values = X, axis = 1)
+X_opt = X[:, [0,1,2,3,4,5,6,7,8,9,10,11]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0,1,2,4,5,6,7,8,9,10,11]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0,1,2,4,6,7,8,9,10,11]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit()
+regressor_OLS.summary()
+X_opt = X[:, [0,1,2,4,6,8,9,10,11]]
+regressor_OLS = sm.OLS(endog= y, exog= X_opt).fit()
+regressor_OLS.summary()
+
+
+# Visualising the Test set results
+plt.scatter(y_pred, y_test, color = 'red')
+plt.title('Predicted vs Actual (Test set)')
+plt.xlabel('Predicted Grade')
+plt.ylabel('Actual Grade')
+plt.show()
